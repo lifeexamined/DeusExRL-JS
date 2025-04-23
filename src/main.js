c5be8b939120947d054070436310f6e1bc452fd7
lifeexamined <nikita.a.novik@gmail.com>
2025-04-23 10:37:46 +0300

    Template

--- src/main.js
+++ src/main.js
@@ -1,21 +1,53 @@
-function main(state) {
-  const terminal = new Terminal();
-  const viewport = new Viewport();
+//Map broken down into an array of strings
+const currentLevel = 0;
+const Map = levels[currentLevel].map;
+const mapLines = levels[currentLevel].map.split("\n");
+const processedMap = [];
+for (let i = 0; i < mapLines.length; i++) {
+  processedMap.push(mapLines[i].split(""));
+}
+console.log("mapLines:");
+console.log(mapLines);
+console.log("processedMap:");
+console.log(processedMap);
+
+function main(State) {
+  Terminal(State);
+  Viewport(State);
 }
 
-const EntityType = ["player", "free", "block", "enemy"];
-class State {
-  constructor() {
-    this.playerPosition = [0, 0, 0];
-    this.cameraDirection = [0, 1, 2, 3];
-    this.enemies = { x, y };
+//Object to store current state of the game
+const State = {
+  playerPosition: [],
+  cameraPosition: [0, 0, 0],
+  cameraDirection: 0,
+  blocks: [],
+  enemies: [[0, 0, 0, 0]],
+};
+
+function initiateState() {
+  for (let i = 0; i < mapLines.length; i++) {
+    for (let j = 0; j < processedMap[i].length; j++) {
+      if (processedMap[i][j] === "@") {
+        State.playerPosition = [j, i, 0];
+      } else if (processedMap[i][j] === "#") {
+        State.blocks.push([i, j, 0]);
+      }
+    }
   }
 }
 
-class Terminal {
-  constructor() {}
-}
+initiateState();
+console.log(State.playerPosition);
+console.log(State.blocks); //Function to initialize the game, takes in the state of the game
 
+//List of all possible entities in the game
+const EntityTypes = ["player", "free", "block", "enemy"];
+
+//Object to store the state of the our imitation of the terminal window
+function Terminal(playerPosition, enemies) {}
+
+//Object that translates the state from the terminal to the 3d viewport
 class Viewport {
   constructor() {
     this.camera = [0, 0, 0];
